dd712d25c8162615e792d8540dc2b6ceef2568c6
Naman <namanmodi260@gmail.com>
2023-08-09 20:42:20 +0530

    added server file

---
+++ controllers/jobController.js
@@ -0,0 +1,35 @@
+import { StatusCodes } from 'http-status-codes';
+import Job from '../models/JobModel.js';
+import { NotFoundError } from '../errors/customErrors.js';
+
+export const getAllJobs = async (req, res) => {
+  const jobs = await Job.find({});
+  res.status(StatusCodes.OK).json({ jobs });
+};
+
+export const createJob = async (req, res) => {
+  const job = await Job.create(req.body);
+  res.status(StatusCodes.CREATED).json({ job });
+};
+
+export const getJob = async (req, res) => {
+  const { id } = req.params;
+  const job = await Job.findById(id);
+  res.status(200).json({ job });
+};
+export const updateJob = async (req, res) => {
+  const { id } = req.params;
+
+  const updatedJob = await Job.findByIdAndUpdate(id, req.body, {
+    new: true,
+  });
+
+  res.status(StatusCodes.OK).json({ job: updatedJob });
+};
+
+export const deleteJob = async (req, res) => {
+  const { id } = req.params;
+  const removedJob = await Job.findByIdAndDelete(id);
+
+  res.status(StatusCodes.OK).json({ job: removedJob });
+};
--- server.js
+++ server.js
@@ -1,8 +1,30 @@
+import 'express-async-errors';
 import express from 'express';
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('hello world');
-});
+import mongoose from 'mongoose';
+import jobRouter from './routes/jobRouter.js';
+import authRouter from './routes/authRouter.js';
+import * as dotenv from 'dotenv';
+import errorHandlerMiddleware from './middleware/errorHandlerMiddleware.js';
+dotenv.config();
 
-app.listen(5100, console.log('server is listening'));
+const port = process.env.PORT || 5100;
+app.use(express.json());
+app.use('/api/v1/jobs', jobRouter);
+app.use('/api/v1/auth', authRouter);
+
+app.use('*', (req, res) => {
+  res.status(404).json({ msg: 'not found' });
+});
+app.use(errorHandlerMiddleware);
+
+try {
+  await mongoose.connect(process.env.MONGO_URL);
+  app.listen(port, () => {
+    console.log(`server running on PORT ${port}....`);
+  });
+} catch (error) {
+  console.log(error);
+  process.exit(1);
+}
